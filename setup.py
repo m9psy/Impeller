f83682a2ddf1776500f2ec676d36a13f35de519f
m9psy <emelianovds@yandex.ru>
2017-05-19 21:27:16 +0300

    Lib path changed

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 import os
+import sys
+import glob
 
+
+language_level = sys.version_info[0]
 _THIS_ = os.path.dirname(os.path.realpath(__file__))
 
-libxlsx_include = os.path.join(_THIS_, "libxlsxwriter", "include")
-libxlsx_lib = os.path.join(_THIS_, "libxlsxwriter", "RESULT", "x64")
-libxlsx_compiled = os.path.join("compiled", "Win", "x64")
-zlib_compiled = os.path.join("compiled", "Win", "x64")
+libxlsx_include = os.path.join(_THIS_, "libs", "XLSX", "include")
+libxlsx_lib = os.path.join(_THIS_, "libs", "XLSX", "lib")
+libxlsx_compiled = os.path.join(_THIS_, "libs", "XLSX", "bin")
+zlib_compiled = os.path.join(_THIS_, "libs", "zlib", "bin")
 
 module_name = "impeller"
 MODULE_PATH = os.path.join(_THIS_, module_name)
@@ -24,45 +28,44 @@ chart = os.path.join(MODULE_PATH, "c_chart.pyx")
 
 extensions = [
     Extension(name="impeller.c_workbook",
-              sources=[workbook],
+              sources=[workbook, worksheet, common, formatting, chart],
               include_dirs=[_THIS_, libxlsx_include, MODULE_PATH],
               library_dirs=[libxlsx_lib],
-              libraries=["xlsxwriter"],
+              libraries=['xlsxwriter'],
               language="c"),
 
     Extension(name="impeller.c_worksheet",
               sources=[worksheet],
               include_dirs=[_THIS_, libxlsx_include, MODULE_PATH],
               library_dirs=[libxlsx_lib],
-              libraries=["xlsxwriter"],
+              libraries=['xlsxwriter'],
               language="c"),
 
     Extension(name="impeller.c_format",
               sources=[formatting],
               include_dirs=[_THIS_, libxlsx_include, MODULE_PATH],
               library_dirs=[libxlsx_lib],
-              libraries=["xlsxwriter"],
+              libraries=['xlsxwriter'],
               language="c"),
 
     Extension(name="impeller.c_common",
               sources=[common],
               include_dirs=[_THIS_, libxlsx_include, MODULE_PATH],
               library_dirs=[libxlsx_lib],
-              libraries=["xlsxwriter"],
+              libraries=['xlsxwriter'],
               language="c"),
 
     Extension(name="impeller.c_chart",
               sources=[chart],
               include_dirs=[_THIS_, libxlsx_include, MODULE_PATH],
               library_dirs=[libxlsx_lib],
-              libraries=["xlsxwriter"],
+              libraries=['xlsxwriter'],
               language="c"),
 ]
 
 setup(
-    name="Impeller first attempt",
+    name="Impeller",
     version='0.1.dev0',
     packages=['impeller'],
-
-    ext_modules=cythonize(extensions, compiler_directives={'language_level': 3})
+    ext_modules=cythonize(extensions, compiler_directives={'language_level': language_level})
 )
