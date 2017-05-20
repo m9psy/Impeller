e55cf068389a9ab65b345217a48ee0009d0e4ea3
m9psy <emelianovds@yandex.ru>
2017-05-20 03:12:48 +0300

    Absolutely not sure, fixing incorrect encoding?

--- impeller/__init__.py
+++ impeller/__init__.py
@@ -1,4 +1,4 @@
-# coding: utf8
+# coding: utf-8
 import os
 
 _THIS_ = os.path.dirname(os.path.realpath(__file__))
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding: utf8
+# coding: utf-8
 
 from setuptools import setup
 from setuptools.extension import Extension
